8aad36d290a766ce843a1ee358b154982cd44c78
JackBlice <abdullamalen@gmail.com>
2024-04-08 12:37:00 +0600

    finish

--- core/urls.py
+++ core/urls.py
@@ -47,6 +47,6 @@ urlpatterns1 = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-   path('products/', include('products.urls')),
+    path('products/', include('products.urls')),
 ] + urlpatterns1
 
